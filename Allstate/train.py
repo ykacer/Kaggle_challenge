61fa15191bda3ca48c96442c00a04fc0429e1008
kyoucef <youcef.kacer@gmail.com>
2016-10-14 22:23:00 +0200

    enable train.py to write sample submission

--- Allstate/train.py
+++ Allstate/train.py
@@ -17,6 +17,13 @@ from sklearn import linear_model
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 
+def write_submission(csv_file,id,loss)
+    dtype = [('id','int32'), ('loss','float32')]
+    values = np.zeros(loss.size,dtype=dtype)
+    values['id'] = id
+    values['loss'] = loss
+    pd.DataFrame(values).to_csv(csv_file,sep=',',index=False)
+
 print("Formatting data")
 
 name_col = list(pd.read_csv('train.csv',header=-1,encoding='utf-8',nrows=1,low_memory=False).values.flatten()) # read header firstly
@@ -30,7 +37,8 @@ X = data[:,1:-1]; # take variables from other columns skipping id column
 
 test_data = pd.read_csv('test.csv',header=0,encoding='utf-8',dtype=name2type_dict) # read test data faster thx to dictionary type
 data = np.array(test_data);
-X_test = data[:,1:];
+id_test = data[:,0]
+X_test = data[:,1:]
 
 print ("Encode categorical variables")
 
@@ -59,6 +67,7 @@ grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
 grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 results.append(['Linear Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,""])
+write_submission('sample_submission_linear_regression.csv',id_test,ytest)
 
 print("* Ridge Regression")
 cl = linear_model.Ridge()
@@ -67,6 +76,7 @@ grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
 grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 results.append(['Ridge Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,""])
+write_submission('sample_submission_ridge_regression.csv',id_test,ytest)
 
 print("* Lasso Regression")
 cl = linear_model.Lasso()
@@ -75,6 +85,7 @@ grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
 grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 results.append(['Lasso Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,""])
+write_submission('sample_submission_lasso_regression.csv',id_test,ytest)
 
 print("* ElasticNet Regression")
 cl = linear_model.ElasticNet()
@@ -83,6 +94,7 @@ grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
 grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 results.append(['ElasticNet Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,""])
+write_submission('sample_submission_elasticnet_regression.csv',id_test,ytest)
 
 print("* Ransac Regression")
 base_estimator = linear_model.LinearRegression(fit_intercept=True)
@@ -91,6 +103,7 @@ cl.fit(Xencode,y)
 ytest = cl.estimator_.predict(Xencode_test)
 best_score_ = cl.score(Xencode[cl.inlier_mask_,:],y[cl.inlier_mask_])
 results.append(['Ransac Regression',[],[],best_score_,[],None,ytest,""])
+write_submission('sample_submission_ransac_regression.csv',id_test,ytest)
 
 print("* Support Vector Regression")
 cl = SVR(kernel='rbf',verbose=True)
@@ -100,11 +113,13 @@ grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 info = "percentage of support vectors : "+1.0*len(grid.best_estimator_.support_)/y.size+"%\n"
 results.append(['Support Vector Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,info])
+write_submission('sample_submission_support_regression.csv',id_test,ytest)
 
 print("* Random Forest Regressor")
 cl = RandomForestRegressor()
-param_grid = {'max_features',['auto']}
+param_grid = {'max_features':['auto']}
 grid = grid_search.GridSearchCV(cl,param_grid,cv=cv,verbose=verbose)
 grid.fit(Xencode,y)
 ytest = grid.best_estimator_.predict(Xencode_test)
 results.append(['Random Forest Regression',grid.grid_scores_,grid.scorer_,grid.best_score_,grid.best_params_,ytest,""])
+write_submission('sample_submission_random_forest_regression.csv',id_test,ytest)
