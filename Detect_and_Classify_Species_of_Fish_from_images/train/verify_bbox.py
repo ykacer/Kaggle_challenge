8b0d57f667190d89be502beafae6e652d3f1d3d5
kyoucef <youcef.kacer@gmail.com>
2017-01-25 22:29:04 +0100

    python file to verify bbox and create ground truth mask

---
+++ Detect_and_Classify_Species_of_Fish_from_images/train/verify_bbox.py
@@ -0,0 +1,31 @@
+import json
+import cv2
+import numpy as np
+
+labels = ['alb','bet','dol','lag','other','shark','yft']
+
+for l in labels:
+    print l.upper()+'...'
+    files = json.load(open(l+'_labels.json'));
+    for f in files:
+        filename = f['filename'];
+        annotations = f['annotations']
+        classe = f['class']
+        image = cv2.imread(l.upper()+'/'+filename,-1);
+        image_mask = np.zeros_like(image);
+        for a in annotations:
+            x = int(a['x'])
+            x = x-x*(x<0)
+            y = int(a['y'])
+            y = y-y*(y<0)
+            h = int(a['height'])
+            w = int(a['width'])
+            c = a['class']
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),3)
+            cv2.rectangle(image_mask,(x,y),(x+w,y+h),(255,255,255),thickness=cv2.cv.CV_FILLED)
+        cv2.imwrite(l.upper()+'/'+filename+'_gt.jpg',image);
+        cv2.imwrite(l.upper()+'/'+filename+'_m.jpg',image_mask.astype(np.uint8));
+
+
+
+
