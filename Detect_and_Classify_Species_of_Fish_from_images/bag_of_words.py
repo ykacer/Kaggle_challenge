bd634c79844b8592c27210142c7c3b945367972e
Youcef Kacer <youssef.kacer@smithsdetection.com>
2017-03-03 20:17:20 +0100

    add data reduction and some hypreparameters classifiers tuning

--- Detect_and_Classify_Species_of_Fish_from_images/bag_of_words.py
+++ Detect_and_Classify_Species_of_Fish_from_images/bag_of_words.py
@@ -7,6 +7,9 @@ import cv2
 from itertools import izip
 
 from sklearn import metrics
+
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
@@ -82,7 +85,7 @@ joblib.dump((dictionary), 'dico_'+descriptor_type+'_bow.pkl', compress=3)
 if descriptor_type == 'sift':
 	descriptor_extractor = cv2.SIFT(nOctaveLayers=4,contrastThreshold=0.02,edgeThreshold=10,sigma=2.1)
 else:
-	descriptor_extractor = cv2.SURF(1000)
+	descriptor_extractor = cv2.SURF(200)
 
 #FLANN_INDEX_KDTREE = 0
 #index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
@@ -114,7 +117,7 @@ for ind,im in enumerate(set_images):
 # record bow features and labels
 bow_features = descriptor_type+'_bow_features.csv'
 pd.DataFrame(X).to_csv(bow_features)
-bow_labels = desriptor_type+'_bow_labels.csv'
+bow_labels = descriptor_type+'_bow_labels.csv'
 pd.DataFrame(y).to_csv(bow_labels)
 
 # Classification
@@ -129,21 +132,33 @@ ytrain = data[:Ntrain,-1]
 Xtest = data[Ntrain:,:-1]
 ytest = data[Ntrain:,-1]
 
+sc = StandardScaler()
+Xtrain_r = sc.fit_transform(Xtrain)
+Xtest_r = sc.transform(Xtest)
+
 n_cl = 7
-n_samples = Xtrain.shape[0] 
-classes = [1,2,3,4,5,6,7]
+n_samples = Xtrain_r.shape[0] 
 C = 1.0
 weights = 1.0*n_samples / (n_cl * np.bincount(ytrain.astype(np.int64)))
-class_weight = dict(zip(classes,weights[1:]))
+class_weight = dict(zip([1,2,3,4,5,6,7],weights[1:]))
 cl = SVC(kernel='rbf',gamma=100.0,decision_function_shape='ovr',probability=True,class_weight=class_weight,random_state=0,verbose=False)
 #cl = SVC(kernel='rbf',random_state=0,verbose=False)
-cl.fit(Xtrain,ytrain)
-print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest)))
+cl.fit(Xtrain_r,ytrain)
+print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest_r)))
 
 cl = KNeighborsClassifier()
-cl.fit(Xtrain,ytrain)
-print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest)))
+cl.fit(Xtrain_r,ytrain)
+print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest_r)))
 
-cl = LogisticRegression(C=75.0,class_weight=class_weight,random_state=0)
-cl.fit(Xtrain,ytrain)
-print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest)))
+cl = LogisticRegression(C=100.0,class_weight=class_weight,random_state=0)
+cl.fit(Xtrain_r,ytrain)
+print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest_r)))
+
+cl = MLPClassifier(hidden_layer_sizes=(2000,500),solver='sgd',activation='relu', batch_size='auto',learning_rate_init=0.1,learning_rate='constant',max_iter=400,random_state=0,tol=0.01,momentum=0.9,nesterovs_momentum=True,early_stopping=True,verbose=True)
+cl.fit(Xtrain_r,ytrain)
+print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest_r)))
+
+cl = GradientBoostingClassifier(learning_rate=0.01, n_estimators=800,max_depth=24, min_samples_split=160, min_samples_leaf=5, subsample=0.8, max_features=11, random_state=0)
+cl.fit(Xtrain_r,ytrain)
+print np.array_str(metrics.confusion_matrix(ytest,cl.predict(Xtest_r)))
+
